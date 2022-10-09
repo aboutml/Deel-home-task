562d8d4ef93e927a9d3ccc9ccb4e23f2050f7075
aboutml <nick.lipnyagov@gmail.com>
2022-10-09 18:33:54 +0300

    Fixed broken api

--- src/app.js
+++ src/app.js
@@ -8,13 +8,13 @@ app.set('sequelize', sequelize);
 app.set('models', sequelize.models);
 
 /**
- * FIX ME!
  * @returns contract by id
  */
 app.get('/contracts/:id', getProfile, async (req, res) => {
     const {Contract} = req.app.get('models');
     const {id} = req.params;
-    const contract = await Contract.findOne({where: {id}});
+    const profile = req.profile;
+    const contract = await Contract.findOne({where: {id, ContractorId: profile.id}});
     if(!contract) return res.status(404).end();
     res.json(contract);
 })
